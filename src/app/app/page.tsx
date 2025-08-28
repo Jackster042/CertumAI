d0237b727c383870c96340d3b7df2dc4d838c1b2
Nemanja Stojanovic <feemail042@gmail.com>
2025-08-28 13:24:54 +0200

    Job Info edit page done

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -46,7 +46,7 @@ async function JobInfos() {
   return (
     <div className="container my-4">
       <div className="flex gap-2 justify-between mb-6">
-        <h1 className="text-3xl md:text-4xl lg:text-5xl">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl mb-4">
           Select a job description
         </h1>
         <Button asChild>
