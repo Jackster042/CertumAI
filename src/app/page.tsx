39a29a990915a7026c5035579e1518785716c1c9
Nemanja Stojanovic <feemail042@gmail.com>
2025-08-30 00:48:54 +0200

    clerk billing setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import { ThemeToggle } from "@/components/ThemeToggle";
+import { PricingTable } from "@/services/clerk/components/PricingTable";
 import { SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function HomePage() {
   // console.log(env.DATABASE_URL);
   return (
     <>
-      <SignInButton />
-      <UserButton />
-      <ThemeToggle />
+      <div className="p-4 space-y-4">
+        <div className="flex items-center gap-4">
+          <SignInButton />
+          <UserButton />
+          <ThemeToggle />
+        </div>
+        <PricingTable />
+      </div>
     </>
   );
 }
