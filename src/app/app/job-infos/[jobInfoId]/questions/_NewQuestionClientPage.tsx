3e756aa50c7659088f802d50e1d64bfd9c66800d
Nemanja Stojanovic <feemail042@gmail.com>
2025-09-14 00:55:25 +0200

    wip

--- src/app/app/job-infos/[jobInfoId]/questions/_NewQuestionClientPage.tsx
+++ src/app/app/job-infos/[jobInfoId]/questions/_NewQuestionClientPage.tsx
@@ -1,5 +1,121 @@
 "use client";
 
-export function NewQuestionClientPage({ jobInfoId }: { jobInfoId: string }) {
-  return <h1>Create a question</h1>;
+import { BackLink } from "@/components/BackLink";
+import { MarkdownRenderer } from "@/components/MarkdownRenderer";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Textarea } from "@/components/ui/textarea";
+import { JobInfoTable } from "@/drizzle/schema";
+import { useState } from "react";
+
+type Status = "awaiting-answer" | "awaiting-difficulty" | "init";
+
+export function NewQuestionClientPage({
+  jobInfo,
+}: {
+  jobInfo: Pick<typeof JobInfoTable.$inferSelect, "id" | "name" | "title">;
+}) {
+  const [status, setStatus] = useState<Status>("init");
+  const [answer, setAnswer] = useState<string | null>(null);
+
+  //   TODO: QUESTION & FEEDBACK
+  const question = null;
+  const feedback = null;
+
+  return (
+    <div className="flex flex-col items-center h-screen-reader gap-4 w-full mx-auto flex-grow max-2-[2000px]">
+      <div className="container flex gap-4 mt-4 items-center justify-between">
+        <div className="flex-grow basis-0">
+          <BackLink href={`/app/job-infos/${jobInfo.id}`}>
+            {jobInfo.name}
+          </BackLink>
+        </div>
+        <Controls />
+        <div className="flex-grow hidden md:block" />
+      </div>
+      <QuestionContainer
+        question={question}
+        feedback={feedback}
+        answer={answer}
+        status={status}
+        setAnswer={setAnswer}
+      />
+    </div>
+  );
+}
+
+function QuestionContainer({
+  question,
+  feedback,
+  answer,
+  status,
+  setAnswer,
+}: {
+  question: string | null;
+  feedback: string | null;
+  answer: string | null;
+  status: Status;
+  setAnswer: (value: string) => void;
+}) {
+  return (
+    <ResizablePanelGroup direction="horizontal" className="flex-grow border-t">
+      <ResizablePanel id="question-and-feedback" defaultSize={50} minSize={5}>
+        <ResizablePanelGroup direction="vertical" className="flex-grow">
+          <ResizablePanel id="question" defaultSize={25} minSize={5}>
+            <ScrollArea className="h-full min-w-48 *:h-full">
+              {status === "init" && question == null ? (
+                <p className="text-base md:text-lg flex items-center justify-center h-full p-6">
+                  Get started by selecting a question difficulty above.
+                </p>
+              ) : (
+                question && (
+                  <MarkdownRenderer className="p-6">
+                    {question}
+                  </MarkdownRenderer>
+                )
+              )}
+            </ScrollArea>
+          </ResizablePanel>
+          {feedback && (
+            <>
+              <ResizableHandle withHandle />
+              <ResizablePanel id="feedback" defaultSize={25} minSize={5}>
+                <ScrollArea className="h-full min-w-48 *:h-full">
+                  <MarkdownRenderer className="p-6">
+                    {feedback}
+                  </MarkdownRenderer>
+                </ScrollArea>
+              </ResizablePanel>
+            </>
+          )}
+        </ResizablePanelGroup>
+      </ResizablePanel>
+
+      <ResizableHandle withHandle />
+      <ResizablePanel id="answer" defaultSize={50} minSize={5}>
+        <ScrollArea className="h-full min-w-48 *:h-full">
+          <Textarea
+            disabled={status !== "awaiting-answer"}
+            onChange={(e) => setAnswer(e.target.value)}
+            value={answer ?? ""}
+            placeholder="Type your answer here..."
+            className="w-full h-full resize-none border-none rounded-none focus-visible:ring focus-visible:ring-inset !text-base p-6"
+          />
+        </ScrollArea>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
+}
+
+function Controls() {
+  return (
+    <div className="flex items-center gap-2">
+      <button className="bg btn-primary">Create</button>
+      <button className="bg btn-primary">Cancel</button>
+    </div>
+  );
 }
