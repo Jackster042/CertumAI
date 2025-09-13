74c4c7e1d1c7f8b462748995823593103fb10f36
Nemanja Stojanovic <feemail042@gmail.com>
2025-09-13 23:49:59 +0200

    new question page init

---
+++ src/app/app/job-infos/[jobInfoId]/questions/_NewQuestionClientPage.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export function NewQuestionClientPage({ jobInfoId }: { jobInfoId: string }) {
+  return <h1>Create a question</h1>;
+}
--- src/app/app/job-infos/[jobInfoId]/questions/page.tsx
+++ src/app/app/job-infos/[jobInfoId]/questions/page.tsx
@@ -1,3 +1,15 @@
+import { db } from "@/drizzle/db";
+import { JobInfoTable } from "@/drizzle/schema";
+import { getJobInfoIdTag } from "@/features/jobInfos/dbCache";
+import { getCurrentUser } from "@/services/clerk/lib/getCurrentUser";
+import { and, eq } from "drizzle-orm";
+import { Loader2Icon } from "lucide-react";
+import { cacheTag } from "next/dist/server/use-cache/cache-tag";
+import { notFound, redirect } from "next/navigation";
+import { Suspense } from "react";
+import { NewQuestionClientPage } from "./_NewQuestionClientPage";
+import { canCreateQuestion } from "@/features/questions/permissions";
+
 export default async function QuestionsPage({
   params,
 }: {
@@ -5,5 +17,37 @@ export default async function QuestionsPage({
 }) {
   const { jobInfoId } = await params;
 
-  return <div>JobInfoQuestionsPage{jobInfoId}</div>;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center h-screen-reader">
+          <Loader2Icon className="animate-spin size-24" />
+        </div>
+      }
+    >
+      <SuspendedComponent jobInfoId={jobInfoId} />
+      {/* <h1>Hello</h1> */}
+    </Suspense>
+  );
+}
+
+async function SuspendedComponent({ jobInfoId }: { jobInfoId: string }) {
+  const { userId, redirectToSignIn } = await getCurrentUser();
+  if (userId == null) return redirectToSignIn();
+
+  if (!(await canCreateQuestion())) return redirect("/app/upgrade");
+
+  const jobInfo = await getJobInfo(jobInfoId, userId);
+  if (jobInfo == null) return notFound();
+
+  return <NewQuestionClientPage jobInfo={jobInfo} />;
+}
+
+async function getJobInfo(jobInfoId: string, userId: string) {
+  "use cache";
+  cacheTag(getJobInfoIdTag(jobInfoId));
+
+  return db.query.JobInfoTable.findFirst({
+    where: and(eq(JobInfoTable.id, jobInfoId), eq(JobInfoTable.userId, userId)),
+  });
 }
